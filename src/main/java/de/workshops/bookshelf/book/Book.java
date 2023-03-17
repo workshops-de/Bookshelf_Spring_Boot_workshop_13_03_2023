073249d02e23fac84a0d61555cf3573c0feda712
Birgit Kratz <mail@birgitkratz.de>
2023-03-17 11:17:37 +0100

    Day 5: Einrichtung und Initialisierung PostgreSQL Datenbank

--- src/main/java/de/workshops/bookshelf/book/Book.java
+++ src/main/java/de/workshops/bookshelf/book/Book.java
@@ -1,10 +1,28 @@
 package de.workshops.bookshelf.book;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String title;
     private String description;
     private String author;
     private String isbn;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
